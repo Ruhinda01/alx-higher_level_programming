694a7ffa679ec1d0cb48f921dfa336a631a58642
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-03 19:36:37 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 
-
 def fizzbuzz():
     for c in range(1, 101):
         if c % 3 == 0 and c % 5 == 0:
@@ -9,9 +8,6 @@ def fizzbuzz():
         elif c % 3 == 0:
             print("Fizz", end=" ")
         elif c % 5 == 0:
-            if c == 100:
-                print("Buzz", end="")
-            else:
-                print("Buzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print("{:d}".format(c), end=" ")
