792417ca440b653cc367f660e83aad208dccfc42
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-09 16:54:25 -0700

    Number 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     newList = my_list[:]
     if idx < 0:
         return newList
-    elif idx > len(newList):
+    elif idx >= len(newList):
         return newList
     else:
         newList[idx] = element
