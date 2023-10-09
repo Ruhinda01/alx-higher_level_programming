f04b7c8e7eceebf2f1d8246c5b8315a4e45e6325
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-09 16:47:04 -0700

    0 - 6

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    newList = my_list[:]
+    if idx < 0:
+        return newList
+    elif idx > len(newList):
+        return newList
+    else:
+        newList[idx] = element
+        return newList
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) != 0:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        for i in range(rows):
+            for j in range(columns):
+                if j == columns - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+    else:
+        print()
