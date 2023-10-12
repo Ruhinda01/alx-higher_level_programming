11a7f99a59b82bd63c35b8c69940786d7edcfdd7
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-11 19:16:30 -0700

    Average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is not None:
+    if my_list is not None and len(my_list) > 0:
         num1 = 0
         num2 = 0
         for c in my_list:
