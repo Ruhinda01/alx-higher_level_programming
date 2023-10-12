373ea3f0e74164f78e0d10f9d33be5bf262bda82
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-11 18:01:08 -0700

    Fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,6 @@ def best_score(a_dictionary):
     if a_dictionary is not None:
         score = None
         key = None
-        
         for k, v in a_dictionary.items():
             if score is None or a_dictionary[k] > score:
                 score = a_dictionary[k]
