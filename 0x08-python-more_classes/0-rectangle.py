9459b1de59bf879e30c01c07df1374b57f847312
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-30 15:12:37 -0700

    Simple

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Rectangle module"""
+
+
+class Rectangle:
+    """
+    This is a class that defines a rectangle
+    """
+    pass
+
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
