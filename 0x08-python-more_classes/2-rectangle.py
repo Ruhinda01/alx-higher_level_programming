a0ed9650f500c2b4e1a8bc2733664d1598b77b20
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-10-30 16:33:29 -0700

    Param-area

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -84,4 +84,6 @@ class Rectangle:
         Return:
             the current perimemter
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
