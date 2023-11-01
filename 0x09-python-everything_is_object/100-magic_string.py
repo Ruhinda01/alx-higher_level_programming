cdf0d0b574c70fa08ae4a851d0b7427e57dec08b
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-11-01 08:04:30 -0700

    Fixes

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(potter=[]):
+    potter.append("BestSchool")
+    return ", ".join(potter)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""LockedClass module"""
+
+
+class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance attributes
+    Except if the new instance attribute is called first_name
+    """
+    __slots__ = ('first_name')
