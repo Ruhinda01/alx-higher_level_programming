9188b0173d5671a1b6372523f9dc51419f34c76f
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-11-07 16:36:43 -0800

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""adds all the arguments to a Python list"""
 import sys
 import json
 import os
 
+
+"""Imports"""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+
 filename = "add_item.json"
 
+"""Checking for the file existence"""
 if not os.path.exists(filename):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write("[]")
