e1c2781203fc7964160f71ccd1f32d698ee39837
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-11-08 13:28:28 -0800

    Student 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary rep of the student instance
+        Args:
+            attr (list)
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the student instance
+        Args:
+            json (dict): dixtionary
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
