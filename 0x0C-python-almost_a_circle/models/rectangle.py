b49b4ac308ef2ab673116b5d39ad1a663321a08b
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2023-11-15 14:24:59 -0800

    Fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Rectangle class
+    Inherits from the Base class
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Instantiation method
