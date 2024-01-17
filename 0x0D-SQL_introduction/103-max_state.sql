478454f1a89fd2dbd493b1cb58f0985a5c6a34ae
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-01-16 16:45:46 -0800

    Final

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- a script that displays the max temperature of each state (ordered by State name).
-SELECT city, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT city, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
