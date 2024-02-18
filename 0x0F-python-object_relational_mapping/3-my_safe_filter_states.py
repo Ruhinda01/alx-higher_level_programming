355f63d4053f52efd75e9a00093dab8873b06ae8
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-02-18 10:33:24 -0800

    Number 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where
+name matches the argument and safe from MySQL injections"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    query = """SELECT * FROM states
+            WHERE BINARY states.name = %s
+            ORDER BY states.id ASC"""
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
