ae66e878039f7817d8acc6f9a4a02886cdb74db2
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-02-18 04:33:53 -0800

    all

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+""" A script that lists all states from the database hbtn_0e_0_usa
+    The script takes 3 arguments: mysql username, mysql password
+    and database name
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) > 3:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT * FROM states
+        ORDER BY states.id ASC
+        """)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
