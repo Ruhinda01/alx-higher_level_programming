b380804bc629208c47aace64e0d1d5c7e6231095
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-01 13:07:19 -0800

    headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
