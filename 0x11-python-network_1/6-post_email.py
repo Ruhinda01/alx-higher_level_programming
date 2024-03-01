6b97c9a3d15c138759df445ef96d91c603ff9ef8
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-01 13:26:25 -0800

    post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter, 
+and finally displays the body of the response
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    payload = {'email': sys.argv[2]}
+    url = sys.argv[1]
+    r = requests.post(url, params=payload)
+    print(r.text)
