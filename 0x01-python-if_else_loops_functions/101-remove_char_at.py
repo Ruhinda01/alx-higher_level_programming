ea283281d96bfa5e8d549b2f4c375f663aafb79d
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-03-18 13:17:36 -0700

    101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that creates a copy of that create a copy
+of the string, removing character at position n
+"""
+
+
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+    new_str = str[:n] + str[n + 1:]
+    return new_str
