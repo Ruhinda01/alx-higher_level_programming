bee16d99703730a63bc6ef20c7a2a14be72aba0a
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-04-04 20:39:46 +0300

    fix

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
-$('document').ready(function () {
+$(document).ready(function () {
   const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
   $.get(url, function (data) {
     $('DIV#hello').text(data.hello);
